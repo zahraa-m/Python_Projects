0028588dbcefc3b458dba50a8167e377632fd275
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-12-17 21:56:45 +0000

    Create 02-Arrays.py

---
+++ 02-Arrays.py
@@ -0,0 +1,22 @@
+#Arrays
+
+#This is a list, changeable, allows duplicate, ordered.
+a=[1,2,3,4,5,6]
+print(a)
+print(type(a))
+
+#This is a tuple, unchangeable, allows duplicate, ordered.
+b=("a","b","c","d","e","f")
+print(b)
+print(type(b))
+
+#This is a set, changeable, no duplicate, unordered.
+c={"0", "1", "2", "3", "4", "5"}
+print(c)
+print(type(c))
+
+#This is a dictionary, unchangeable, allows duplicate, ordered.
+d={"a":1 ,"b":2 ,"c":3 ,"d":4 ,"e":5 ,"f":6}
+print(d)
+print(type(d))
+
