35fc5bc9a0386bce6ed28ffabe1916fbba35493f
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2019-12-22 23:58:45 +0000

    Update 06-Lambda.py

--- 06-Lambda.py
+++ 06-Lambda.py
@@ -11,3 +11,13 @@ print(fun1(3))
 #this lambda is the same as fun1
 print((lambda i:i+3)(3))
 
+#print only even numbers in the list
+a = [1, 2, 3, 4, 5]
+ev = list(filter(lambda x: x%2==0, a))
+print(ev)
+
+#sub 2
+c = [1, 2, 3, 4, 5]
+s = list(map(lambda x: x-2, c))
+print(s)
+
