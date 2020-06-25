e80fca08f03439b2f43c6349cfeb623e29e21792
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2020-06-25 03:52:29 +0100

    Create 09-read_write_files.py

---
+++ 09-read_write_files.py
@@ -0,0 +1,19 @@
+#read the song Bohemian Rhapsody by Queen
+song=open('Bohemian_Rhapsody-Queen.txt', "r")
+print(song.read())
+song.close()
+
+
+#open file by With
+with open('Bohemian_Rhapsody-Queen.txt', "r") as song1:
+
+    for n in range(4):
+        lyrics = song1.readline()
+        print(lyrics)
+
+
+
+
+#write your name into a text file
+test=open('writing_test.txt','w')
+test.write("Hi, my name is Zahraa")
