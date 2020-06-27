e39ebcf25cabc4d0a19dbf3ec851b90242800459
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2020-06-27 16:12:13 +0100

    Update 10-Pandas.py

--- 10-Pandas.py
+++ 10-Pandas.py
@@ -4,9 +4,15 @@ adm=pd.read_csv('Admission_Predict.csv')
 adm.head()
 print(adm)
 
-#loc
-print(adm.loc[3,'GRE Score'])
+# loc
+print(adm.loc[3, 'GRE Score'])
 
-#iloc
-print(adm.iloc[3,1])
-print(adm.iloc[0:3,0:3])
+# iloc
+print(adm.iloc[3, 1])
+print(adm.iloc[0:3, 0:3])
+
+# find the unique values in GRE score
+GRE = adm['GRE Score'].unique()
+print(GRE)
+print(len(GRE))
+
