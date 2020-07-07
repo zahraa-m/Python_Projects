638d97dd795bfe5595bf3176cef0a054a9cbf7d8
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2020-07-07 01:46:19 +0100

    Create 10-Pandas.py

---
+++ Python_Basics/10-Pandas.py
@@ -0,0 +1,26 @@
+# pandas is a library written for the Python for data manipulation and analysis
+import pandas as pd
+adm=pd.read_csv('Admission_Predict.csv')
+adm.head()
+print(adm)
+
+# loc
+print(adm.loc[3, 'GRE Score'])
+
+# iloc
+print(adm.iloc[3, 1])
+print(adm.iloc[0:3, 0:3])
+
+# find the unique values in GRE score
+GRE = adm['GRE Score'].unique()
+print(GRE)
+print(len(GRE))
+
+# find GRE value >= 337
+print(adm['GRE Score'] >= 337)
+adm1 = adm[adm['GRE Score'] >= 337]
+print(adm1)
+
+# Save GRE Values >= 337 to new CSV file
+adm1.to_csv('GRE_Scores_eg_to_337.csv')
+
