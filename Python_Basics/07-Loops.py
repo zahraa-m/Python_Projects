17442ecb5fb3a9936859d46726cc52ad0364362c
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2020-07-07 01:43:51 +0100

    Create 07-Loops.py

---
+++ Python_Basics/07-Loops.py
@@ -0,0 +1,15 @@
+#print out the elements in the list A
+A=[1,2,3,4,5]
+for x in A:
+    print(x)
+
+#While as loop and condition function :)
+z=11
+y=1
+while (y< z):
+    print(y)
+    y=y+1
+
+#range function
+for s in range(10):
+    print(s)
