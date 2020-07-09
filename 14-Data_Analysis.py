743f6e07d13faadae3df1384efac6513f60545ee
Zahraa Abdulhussein <abdulhusseinzm@gmail.com>
2020-07-10 00:32:45 +0100

    Update 14-Data_Analysis.py

--- 14-Data_Analysis.py
+++ 14-Data_Analysis.py
@@ -11,4 +11,26 @@ print(df.dtypes)
 # Calculate Correlation
 print(df.corr)
 
+# calculate correlation for bore, stroke, compression-ratio and horsepower
+df[['bore', 'stroke', 'compression-ratio', 'horsepower']].corr()
+
+# Obtain the linear relationship between engine-size and price
+sns.regplot(x="engine-size", y="price", data=df)
+plt.ylim(0,)
+plt.show()
+print(df[["engine-size", "price"]].corr())
+
+# Obtain the linear relationship between highway-mpg and price
+sns.regplot(x="highway-mpg", y="price", data=df)
+plt.show()
+print(df[["highway-mpg", "price"]].corr())
+
+# Obtain the linear relationship between stroke and price
+sns.regplot(x="stroke", y="price", data=df)
+plt.show()
+print(df[['stroke', 'price']].corr())
+
+
+
+
 
